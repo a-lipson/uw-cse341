6e186b5451a8665a1b5702ab7e3bb9f594443c37
dre <a@alipson.me>
2024-11-03 12:36:50 -0800

    work on hw4 from last time

--- homework/hw4/hw4.ml
+++ homework/hw4/hw4.ml
@@ -12,20 +12,51 @@ let list_nth_mod xs n =
   else if xs = [] then raise EmptyList 
   else List.nth xs (n mod List.length xs)
 
+(* assume k non-negative *)
 let rec stream_first_k_such_that p k (Stream t) =
-  failwith "stream_first_k_such_that: not implemented"
+  if k = 0 then [] else
+  let h, r = t () in 
+  if p h then h :: stream_first_k_such_that p (k-1) r 
+  else stream_first_k_such_that p k r
+
+let rec stream_map f (Stream s) = 
+  let (x, s') = s () in 
+  Stream (fun () -> (f x, stream_map f s'))
+
+(* helper stream *)
+let nats = 
+  let rec go n = (n, Stream (fun () -> go (n + 1)))
+  in Stream (fun () -> go 0)
+
 
 let funny_number_stream : int stream =
-  Stream (fun () -> failwith "funny_number_stream: not implemented")
+  let f x = if x mod 6 = 0 then -x else x
+  in stream_map f nats
+  (* let f x = if x mod 6 = 0 then -x else x in  *)
+  (* let rec next n = (f n, Stream (fun () -> next (n + 1))) *)
+  (* in Stream (fun () -> next 1) *)
 
 let foo_then_bar : string stream =
-  Stream (fun () -> failwith "foo_then_bar: not implemented")
+  let f x = if x mod 2 = 0 then "foo" else "bar" 
+  in stream_map f nats
+  (* let rec next b = ((if b then "foo" else "bar"),  *)
+  (*                   Stream (fun () -> next (not b))) *)
+  (* in Stream (fun () -> next true) *)
 
-let stream_pair_all_with_one s =
-  failwith "stream_pair_all_with_one: not implemented"
+let stream_pair_all_with_one s = stream_map (fun x -> (1, x)) s
 
-let cycle_lists xs ys = failwith "cycle_lists: not implemented"
+(* assume both lists non-empty *)
+let cycle_lists xs ys = 
+  let f x = (list_nth_mod xs x, list_nth_mod ys x)
+  in stream_map f nats
+  (* let rec next n = ((list_nth_mod xs n, list_nth_mod ys n),  *)
+  (*                   Stream (fun () -> next (n+1))) *)
+  (* in Stream (fun () -> next 0)  *)
+
 let array_assoc key a = failwith "array_assoc: not implemented"
+
 let caching_assoc xs n = failwith "caching_assoc: not implemented"
+
 let tokenize s = failwith "tokenize: not implemented"
+
 let rec interpret stack ts = failwith "interpret: not implemented"
--- homework/hw4/hw4test.ml
+++ homework/hw4/hw4test.ml
@@ -22,3 +22,30 @@ let%test _ = try
 let%test _ = list_nth_mod ["a";"b"] 2 = "a"
 let%test _ = list_nth_mod ["a";"b"] 4 = "a"
 let%test _ = list_nth_mod ["a";"b"] 1 = "b"
+
+(* 4 *)
+let rec ones = Stream (fun () -> (1,ones))
+
+let nats = 
+  let rec go start = (start, Stream (fun () -> go (start + 1)))
+  in Stream (fun () -> go 0)
+
+let%test _ = stream_first_k_such_that (fun x -> true) 0 ones = []
+let%test _ = stream_first_k_such_that (fun x -> x = 1) 2 ones = [1;1]
+let%test _ = stream_first_k_such_that (fun x -> x mod 2 = 0) 3 nats = [0;2;4]
+
+(* 5 *)
+let%test _ = stream_first_k_such_that (fun x -> x > 0) 6 funny_number_stream = [1;2;3;4;5;7]
+
+(* 6 *)
+let%test _ = stream_first_k_such_that (fun x -> true) 3 foo_then_bar = ["foo";"bar";"foo"] 
+
+(* 7 *)
+let%test _ = stream_first_k_such_that 
+  (fun x -> true) 2 (stream_pair_all_with_one foo_then_bar) = [(1,"foo");(1,"bar")]
+
+(* 8 *)
+let%test _ = stream_first_k_such_that 
+  (fun x -> true) 8 (cycle_lists [1;2;3] ["a";"b"]) =
+  [(1, "a"); (2, "b"); (3, "a"); (1, "b"); (2, "a"); (3, "b"); (1, "a"); (2, "b")]
+
