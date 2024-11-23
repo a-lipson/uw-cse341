08c66165090934a2eb7730d19f1712425387c30a
dre <a@alipson.me>
2024-11-22 16:18:56 -0800

    fix let binding check for duplicates

--- homework/hw6/src/ast.ml
+++ homework/hw6/src/ast.ml
@@ -1,12 +1,15 @@
 include Ast_types
 open Errors
 
+let fmap f (x, y) = (f x, f y)
+let (<$>) = fmap
+
 (* last stage of parser: converts pst to expr *)
 let rec expr_of_pst p =
   let raise_expect_args n info = 
     raise (AbstractSyntaxError (info ^ " expects " ^ string_of_int n ^ " args but got " ^ Pst.string_of_pst p))
-  in let raise_detail_expr_from_pst p info = 
-    raise (AbstractSyntaxError (info ^ (p |> expr_of_pst |> string_of_expr)))
+  in let raise_explain_pst info pst =
+    raise (AbstractSyntaxError (info ^ Pst.string_of_pst pst))
   in let op    sym c = function (* args = match args with *)
     | [p] -> c (expr_of_pst p)
     | _   -> raise_expect_args 1 ("operator " ^ sym)
@@ -28,7 +31,7 @@ let rec expr_of_pst p =
   | Pst.Node [] -> raise (AbstractSyntaxError "Expected expression but got '()'")
   | Pst.Node (head :: args) ->
       match head, args with
-      | Pst.Node _, _ -> raise (AbstractSyntaxError ("Expression forms must start with a symbol, but got " ^ Pst.string_of_pst head))
+      | Pst.Node _, _ -> raise_explain_pst "Expression forms must start with a symbol, but got " head
 
       | Pst.Symbol "car"  , args -> op    "car"   (fun x   -> Car       x) args (* imagine if constructors were curried functions... *)
       | Pst.Symbol "cdr"  , args -> op    "cdr"   (fun x   -> Cdr       x) args
@@ -44,34 +47,36 @@ let rec expr_of_pst p =
       | Pst.Symbol "if"   , _                  -> raise_expect_args 3 "if expression" 
 
       | Pst.Symbol "let"  , [Pst.Node defs; body] -> 
-          (* could catch duplicate error prior to parsing all pst defs *)
-          let bind = function
-            | Pst.Node [Pst.Symbol name; pst] -> 
-                (* let _ = print_endline ("attempting to bind " ^ name ^ " with " ^ (pst |> expr_of_pst |> string_of_expr)) in *)
-                (name, expr_of_pst pst)
-            | p -> raise_detail_expr_from_pst p "Malformed binding in let expression"
-          in let has_dups l = 
-            let sorted = List.sort_uniq String.compare (List.map fst l) in
-            List.length sorted < List.length l
-          (* in let rec bindings acc = function  *)
-          (*   | []  ->  *)
-          (*       let _ = print_endline ("returning bindings") in  *)
-          (*       acc  *)
-          (*   | def :: defs  -> let (name, expr) as b = bind def in *)
-          (*     if not (List.exists (fun (n, _) -> n = name) acc)  *)
-          (*     then  *)
-          (*       let _ = print_endline ("adding " ^ fst b ^ show_expr (snd b)) in *)
-          (*       bindings (b :: acc) defs *)
-          (*     else  *)
-          (*       raise_detail_expr_from_pst def "Duplciate binding in let expression " *)
-          in let bindings = List.map bind defs in 
-          if has_dups bindings then raise (AbstractSyntaxError ("Duplicate binding in let expression " ^ Pst.string_of_pst p))
-          else Let (bindings, expr_of_pst body)
+          let bind_def seen = function
+          | Pst.Node [Pst.Symbol name; pst] -> 
+              if List.mem name seen 
+              then raise_explain_pst ("Duplicate binding: " ^ name) p
+              else (name :: seen, (name, expr_of_pst pst)) 
+          | p -> raise_explain_pst "Malformed binding in let expression" p
+          in let (_, bindings) = List.fold_left_map bind_def [] defs
+          in Let (bindings, expr_of_pst body)
+          (* let bind = function *)
+          (*   | Pst.Node [Pst.Symbol name; pst] ->  *)
+          (*       (name, expr_of_pst pst) *)
+          (*   | p -> raise_explain_pst "Malformed binding in let expression" p *)
+          (* in let has_dups l = *)
+          (*   (* |> List.map fst  *) *)
+          (*   (* |> fun l -> (l, List.sort_uniq String.compare) *) *)
+          (*   (* |> <$> List.length  *) (* (<$>) fmap into pair functorial context *) *)
+          (*   (* |> uncurry (<>)  *) *)
+          (*   let sorted_unique = List.sort_uniq String.compare (List.map fst l) in *)
+          (*   List.length sorted_unique < List.length l *)
+          (* in let bindings = List.map bind defs in  *)
+          (* (* could catch duplicate error prior to parsing all pst defs *) *)
+          (* if has_dups bindings then raise_explain_pst "Duplicate binding in let expression" p *)
+          (* else Let (bindings, expr_of_pst body) *)
       | Pst.Symbol "let"  , _ -> raise_expect_args 2 "let expression"
 
-      | Pst.Symbol "cond" , [] ->  raise (AbstractSyntaxError "not implemented")
-      | Pst.Symbol "cond" , _ -> raise (AbstractSyntaxError "not implemented")
-
+      | Pst.Symbol "cond" , clauses ->  
+          let eval = function 
+            | Pst.Node [pred; body] -> (expr_of_pst pred, expr_of_pst body)
+            | p -> raise_explain_pst "Malformed clause in cond expression " p
+          in Cond (List.map eval clauses)
       | Pst.Symbol f, [] ->  raise (AbstractSyntaxError "not implemented")
 
       | Pst.Symbol s, _ -> raise (AbstractSyntaxError ("Unknown symbol " ^ s))
